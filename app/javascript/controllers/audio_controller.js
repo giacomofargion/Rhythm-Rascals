49b8f34b2b55940e28da64b23f3e5401ef6438cc
Asad Mirza <asadneo@yahoo.com>
2024-05-17 10:47:12 +0100

    Show Page

--- app/javascript/controllers/audio_controller.js
+++ app/javascript/controllers/audio_controller.js
@@ -1,32 +1,59 @@
 import { Controller } from "@hotwired/stimulus";
-import {Howl, Howler} from 'howler';
+import { Howl, Howler } from 'howler';
 
-
-// Connects to data-controller="tone"
+// Connects to data-controller="audio"
 export default class extends Controller {
+  static values = {
+    urls: Array // Assume this comes as an array of URLs
+  }
 
-    static values = {
-      urls: Array // Assume this comes as an array of URLs
-    }
+  static targets = ["avatar", "button"]
 
   connect() {
-    console.log("hello")
+    console.log("hello");
     this.sounds = this.urlsValue.map(url => new Howl({
       src: [url],
       volume: 1,
       loop: true,
       mute: true
     }));
+    this.isPlaying = false; // Track whether the sounds are playing
   }
 
-    playAll() {
+  playAll() {
+    if (this.isPlaying) {
+      this.stopAll();
+    } else {
       this.sounds.forEach((sound) => sound.play());
+      this.startJumping();
+      this.buttonTarget.textContent = "Stop";
     }
-
-    unmute(event) {
-      const index = event.currentTarget.dataset.index;
-      console.log(index)
-      const sound = this.sounds[index];
-      sound.mute(!sound.mute());
-    }
+    this.isPlaying = !this.isPlaying;
   }
+
+  stopAll() {
+    this.sounds.forEach((sound) => sound.stop());
+    this.stopJumping();
+    this.buttonTarget.textContent = "Start";
+  }
+
+  unmute(event) {
+    const index = event.currentTarget.dataset.index;
+    console.log(index);
+    const sound = this.sounds[index];
+    sound.mute(!sound.mute());
+
+  }
+
+  startJumping() {
+    this.avatarTargets.forEach(avatar => {
+      avatar.classList.add('jump');
+    });
+  }
+
+  stopJumping() {
+    this.avatarTargets.forEach(avatar => {
+      avatar.classList.remove('jump');
+    });
+  }
+}
